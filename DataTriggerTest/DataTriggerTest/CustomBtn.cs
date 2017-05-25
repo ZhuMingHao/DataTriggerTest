8dffe5bdad178e64b98296732e1bfa4c7b54e651
Nico Zhu <v-zhumin@microsoft.com>
2017-05-25 15:24:28 +0800

    Add project files.

---
+++ DataTriggerTest/DataTriggerTest/CustomBtn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataTriggerTest
+{
+   public class CustomBtn : Button
+    {public CustomBtn()
+        {
+            this.Clicked += CustomBtn_Clicked;
+        }
+
+        private void CustomBtn_Clicked(object sender, EventArgs e)
+        {
+            IsSelected = !IsSelected;
+        }
+
+        private static readonly BindableProperty IsSelectedProperty = BindableProperty.Create(
+      propertyName: "IsSelected",
+      returnType: typeof(bool),
+      declaringType: typeof(CustomBtn),
+      defaultValue: default(bool));
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+    }
+}
---
+++ DataTriggerTest/DataTriggerTest/HomePageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataTriggerTest
+{
+    public class HomePageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            this.PropertyChanged(this,new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Command _trigger;
+        public Command Trigger
+        {
+            get
+            {
+                return _trigger
+                    ?? (_trigger = new Command(
+                    () =>
+                    {
+                        IsSelected = !IsSelected;
+                    }));
+            }
+        }
+    }
+}
